07e5de88a22cf3f6c2d052a2b010840d00b09426
kecheon <kecheon@gmail.com>
2014-08-30 00:21:38 +0900

    minimal http req and response

--- functional_test.py
+++ functional_test.py
@@ -7,7 +7,7 @@ import unittest
 class NewVisitorTest(unittest.TestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
-        self.browser.implicitly_wait(self, 3)
+        self.browser.implicitly_wait(3)
 
     def tearDown(self):
         self.browser.quit()
