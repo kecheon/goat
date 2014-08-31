59d325ae9c22ce2672927bc4d17dc3a7f2e2dc97
kecheon <kecheon@gmail.com>
2014-08-31 14:39:27 +0900

    redirect and show all items in table

--- lists/tests.py
+++ lists/tests.py
@@ -26,6 +26,36 @@ class ItemTest(TestCase):
         second_saved_item = saved_items[1]
         self.assertEqual(u'쌈도 샀니?', second_saved_item.text)
 
+    def test_save_POST_request(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = u'족발과 새우젓'
+        response = home_page(request)
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, u'족발과 새우젓')
+        # self.assertIn(u'족발과 새우젓', response.content.decode())
+        # expected_html = render_to_string('home.html',
+        #                                  {'new_item_text':u'족발과 새우젓'})
+        # self.assertEqual(response.content.decode(), expected_html)
+
+    def test_post_redirect(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = u'족발과 새우젓'
+        response = home_page(request)
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
+
+
+    def test_save_only_not_none(self):
+        request = HttpRequest()
+        home_page(request)
+        self.assertEqual(Item.objects.count(), 0)
+
+
+
 class HomePageTest(TestCase):
 
     def test_root_url_resolves_to_homepage_view(self):
@@ -38,11 +68,11 @@ class HomePageTest(TestCase):
         expected_html = render_to_string("home.html")
         self.assertTrue(response.content.decode(), expected_html)
 
-    def test_post_request(self):
+    def test_display_all_items(self):
+        Item.objects.create(text='Itemy 1')
+        Item.objects.create(text='Itemy 2')
         request = HttpRequest()
-        request.method = 'POST'
-        request.POST['item_text'] = u'족발과 새우젓'
         response = home_page(request)
 
-        self.assertIn(u'족발과 새우젓', response.content.decode())
-
+        self.assertIn('Itemy 1', response.content.decode())
+        self.assertIn('Itemy 2', response.content.decode())
--- lists/views.py
+++ lists/views.py
@@ -1,9 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from lists.models import Item
 # Create your views here.
 
 
 def home_page(request):
-
-    return render(request, 'home.html',
-                  {'new_item_text':request.POST.get('item_text', '')})
+    new_item = Item()
+    if request.method == 'POST':
+        new_item.text = request.POST.get('item_text')
+        new_item.save()
+        return redirect('/')
+    items = Item.objects.all()
+    return render(request, 'home.html', {'items':items})
