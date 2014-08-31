501e1a3e95b8c8cd9b91c018f4f7e3f4e6b4e132
kecheon <kecheon@gmail.com>
2014-08-31 11:44:44 +0900

    persist model implemented

--- lists/tests.py
+++ lists/tests.py
@@ -3,15 +3,28 @@ from django.core.urlresolvers import resolve
 from django.http import HttpRequest
 from lists.views import home_page
 from django.template.loader import render_to_string
+from lists.models import Item
 
 # Create your tests here.
 
+class ItemTest(TestCase):
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = u'족발과 새우젓'
+        first_item.save()
 
-# class SmokeTest(TestCase):
-#
-#     def test_bad_maths(self):
-#         self.assertEqual(1+1, 3)
+        second_item = Item()
+        second_item.text = u'쌈도 샀니?'
+        second_item.save()
 
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        self.assertEqual(u'족발과 새우젓', first_saved_item.text)
+
+        second_saved_item = saved_items[1]
+        self.assertEqual(u'쌈도 샀니?', second_saved_item.text)
 
 class HomePageTest(TestCase):
 
