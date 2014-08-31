c749efa3bc59c4e55eed943904df23ad359f22f1
kecheon <kecheon@gmail.com>
2014-08-31 15:05:58 +0900

    functional test as app

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -2,10 +2,10 @@ __author__ = 'cheon'
 #-*-coding:utf8-*-
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import unittest
+from django.test import LiveServerTestCase
 
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(3)
@@ -20,7 +20,7 @@ class NewVisitorTest(unittest.TestCase):
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # 유저가 사이트 방문
-        self.browser.get("http://localhost:8000")
+        self.browser.get(self.live_server_url)
         # title과 제목에 'TODO'가 있네...
         self.assertIn('TODO', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
