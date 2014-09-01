577f7868f5208e5763325d6e67afd2f0d4494b90
kecheon <kecheon@gmail.com>
2014-09-01 14:54:28 +0900

    association to list

--- lists/tests.py
+++ lists/tests.py
@@ -3,27 +3,37 @@ from django.core.urlresolvers import resolve
 from django.http import HttpRequest
 from lists.views import home_page
 from django.template.loader import render_to_string
-from lists.models import Item
+from lists.models import Item, List
 
 # Create your tests here.
 
-class ItemTest(TestCase):
+class ListAndItemModelsTest(TestCase):
     def test_saving_and_retrieving_items(self):
+        list_ = List()
+        list_.save()
         first_item = Item()
         first_item.text = u'족발과 새우젓'
+        first_item.list = list_
         first_item.save()
 
         second_item = Item()
         second_item.text = u'쌈도 샀니?'
+        second_item.list = list_
         second_item.save()
+
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, list_)
+
 
         saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(), 2)
 
         first_saved_item = saved_items[0]
         self.assertEqual(u'족발과 새우젓', first_saved_item.text)
+        self.assertEqual(list_, first_saved_item.list)
 
         second_saved_item = saved_items[1]
+        self.assertEqual(list_, second_saved_item.list)
         self.assertEqual(u'쌈도 샀니?', second_saved_item.text)
 
 
@@ -58,8 +68,9 @@ class ListViewTest(TestCase):
         self.assertTemplateUsed(response, 'list.html')
 
     def test_displays_all_items(self):
-        Item.objects.create(text='Itemy 1')
-        Item.objects.create(text='Itemy 2')
+        list_ = List.objects.create()
+        Item.objects.create(text='Itemy 1', list=list_)
+        Item.objects.create(text='Itemy 2', list=list_)
 
         response = self.client.get('/lists/the-unique-url/')
         self.assertContains(response, 'Itemy 1')
