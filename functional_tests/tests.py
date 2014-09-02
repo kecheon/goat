9cab0b95d8dc27e44dbbfd4b8fc1c769607086ac
kecheon <kecheon@gmail.com>
2014-09-02 20:13:55 +0900

    1st step to test styling and layout

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -71,5 +71,26 @@ class NewVisitorTest(LiveServerTestCase):
 
         # self.fail("End of tests!")
 
+
+    def test_layout_and_styling(self):
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(inputbox.location['x']
+                               + inputbox.size['width']/2,
+                               512,
+                               delta=5
+        )
+        inputbox.send_keys(u'껍데기도\n')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(inputbox.location['x']
+                               + inputbox.size['width']/2,
+                               512,
+                               delta=5
+        )
+
+
+
 if __name__ == "__main__":
     unittest.main(warnings='ignore')
