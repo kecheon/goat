7f82fa50d15863e3a0349f50ac40412b351eb4e4
kecheon <kecheon@gmail.com>
2014-09-04 02:36:57 +0900

    no server_thread error fix

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -91,7 +91,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
 
     def test_layout_and_styling(self):
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         self.browser.set_window_size(1024, 768)
 
         inputbox = self.browser.find_element_by_id('id_new_item')
