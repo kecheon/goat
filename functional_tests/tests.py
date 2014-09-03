b1bb6bb9da31673fc0906a3da91dc28944fb9a76
kecheon <kecheon@gmail.com>
2014-09-03 09:12:12 +0900

    bootstrap layout improve

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -2,10 +2,10 @@ __author__ = 'cheon'
 #-*-coding:utf8-*-
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(3)
